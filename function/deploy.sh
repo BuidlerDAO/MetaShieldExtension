0085fad9eaa23a4c1634a8037c79cf0e07081c98
Diven <aboutmydreams@163.com>
2022-06-08 14:52:33 +0800

    fix: update deploy.sh

--- function/deploy.sh
+++ function/deploy.sh
@@ -2,4 +2,4 @@
 # .env Document: https://cloud.tencent.com/document/product/583/44786
 # TENCENT_SECRET_ID=xxxxxxxxxx
 # TENCENT_SECRET_KEY=xxxxxxxx
-export SERVERLESS_PLATFORM_VENDOR=tencent sls deploy && tsc && sls deploy
+export SERVERLESS_PLATFORM_VENDOR=tencent && tsc && sls deploy
